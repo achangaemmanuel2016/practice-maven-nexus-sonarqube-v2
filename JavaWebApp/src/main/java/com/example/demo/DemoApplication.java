c8ec3c95ee90c07740d1fade658111e3c758b239
ACHANGA EMMANUEL EBOT <achangaemmanuel2016@gmail.com>
2023-05-30 06:21:20 -0400

    modified app code

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		string name = "ebot";
+
+		system.out.print(name);
+
+		
 	}
 
 }
